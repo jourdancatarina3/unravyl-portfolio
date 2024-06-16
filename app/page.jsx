d0501b7049fd20ee0f070ea353eee6fb696dbe18
Yunjin <jed.donaire08@gmail.com>
2024-06-16 19:55:53 +0800

    [Chore][Jed] - Added slider

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ProfileSlider from "@/components/ProfileSlider";
+
+export default function Home() {
+  return (
+    <main className="h-full">
+      <ProfileSlider />
+    </main>
+  );
+}
