e347f88b7dbc15bfbf130195d736f541550cacb4
Yunjin <jed.donaire08@gmail.com>
2024-06-17 20:57:25 +0800

    [Modification] - Modified portfolio

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
 import { projects } from "../constants";
 import Image from "next/image";
 import Link from "next/link";
 
 function AboutUs() {
+  useEffect(() => {
+    const cards = document.querySelectorAll(".portfolios");
+
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    }, observerOptions);
+
+    cards.forEach((card) => {
+      observer.observe(card);
+    });
+
+    return () => {
+      cards.forEach((card) => {
+        observer.unobserve(card);
+      });
+    };
+  }, []);
+
   return (
-    <section className="flex items-center relative justify-center flex-col mt-[5rem]">
+    <section
+      id="projects"
+      className="flex items-center relative justify-center flex-col mt-[5rem]"
+    >
       <div class="backgrounds z-0 absolute top-[5rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex opacity-65 items-center justify-center"></div>
       <h1 className="text-[5rem] font-black header">Projects</h1>
       <div className="flex items-center gap-5 w-3/4 mt-[5rem] justify-center">
         {projects.map((proj) => (
           <div
             key={proj.name}
-            className="bg-[#554E4E] w-1/2 rounded-xl flex items-center flex-col justify-center"
+            className="bg-[#554E4E] portfolios w-1/2 rounded-xl flex items-center flex-col justify-center"
           >
             <div className="image-container   rounded-t-xl flex items-center mt-[0.7rem] justify-center">
               <Image
