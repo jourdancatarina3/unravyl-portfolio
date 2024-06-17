b78553777f9011f664085c44fe8f8d6950f617ed
Yunjin <jed.donaire08@gmail.com>
2024-06-17 12:58:35 +0800

    [Modification][Jed] - Added components in Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import ProfileSlider from "@/components/ProfileSlider";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import TechStack from "@/components/TechStack";
+import AboutUs from "@/components/AboutUs";
 
 export default function Home() {
   return (
-    <main className="h-full">
-      <ProfileSlider />
+    <main className="flex items-center justify-center flex-col ">
+      <Navbar />
+      <Hero />
+      <TechStack />
+      <AboutUs />
+      <div className="w-full h-[60rem]">
+        <ProfileSlider />
+      </div>
+      <div className="h-[50rem] flex items-center">
+        <p>hi</p>
+      </div>
     </main>
   );
 }
