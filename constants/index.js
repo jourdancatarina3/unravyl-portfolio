0ea369b5f70e65f57d92e9edb17dc6683a86e05b
Yunjin <jed.donaire08@gmail.com>
2024-06-17 13:00:44 +0800

    [Feat][Jed] - Added tech stack info and images

---
+++ constants/index.js
@@ -0,0 +1,89 @@
+export const navLinks = [
+  {
+    id: "About Us",
+    title: "About Us",
+  },
+  {
+    id: "Projects",
+    title: "Projects",
+  },
+  {
+    id: "Services",
+    title: "Services",
+  },
+  {
+    id: "Contacts",
+    title: "Contacts",
+  },
+];
+
+export const stacks = [
+  { source: "/vuejs.svg", id: "vuejs", name: "VueJS", use: "Frontend" },
+  { source: "/js.svg", id: "js", name: "Javascript", use: "Frontend" },
+  { source: "/react.png", id: "react", name: "ReactJs", use: "Frontend" },
+  { source: "/nextjs.svg", id: "nextjs", name: "NextJs", use: "Fullstack" },
+  { source: "/nodejs.svg", id: "nodejs", name: "NodeJs", use: "Backend" },
+  { source: "/c++.png", id: "c++", name: "C++", use: "Backend" },
+  { source: "/python.png", id: "python", name: "Python", use: "Backend" },
+  {
+    source: "/rstud.svg",
+    id: "rstud",
+    name: "Rstudio",
+    use: "Data Science",
+  },
+  {
+    source: "/tailwind.png",
+    id: "tailwind",
+    name: "Tailwind",
+    use: "Frontend",
+  },
+  {
+    source: "/expressjs.svg",
+    id: "expressjs",
+    name: "ExpressJs",
+    use: "Backend",
+  },
+  {
+    source: "/django.svg",
+    id: "django",
+    name: "Django",
+    use: "Backend",
+  },
+  { source: "/mongodb.svg", id: "mongodb", name: "Mongodb", use: "Database" },
+  {
+    source: "/postgresql.svg",
+    id: "postgresql",
+    name: "Postgresql",
+    use: "Database",
+  },
+  {
+    source: "/typescript.svg",
+    id: "typescript",
+    name: "Typescript",
+    use: "Frontend Extension",
+  },
+  {
+    source: "/swift.svg",
+    id: "typescript",
+    name: "Typescript",
+    use: "Mobile Application",
+  },
+  {
+    source: "/aws.svg",
+    id: "typescript",
+    name: "Cloud",
+    use: "Storage and Deployment",
+  },
+  {
+    source: "/fastapi.svg",
+    id: "fastapi",
+    name: "FastApi",
+    use: "Backend Framework",
+  },
+  {
+    source: "/nuxt.svg",
+    id: "nuxt",
+    name: "Nuxt",
+    use: "Frontend Framework",
+  },
+];
