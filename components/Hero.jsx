38cdb88da90ce819d31135d62236c7ae046d7e48
Yunjin <jed.donaire08@gmail.com>
2024-06-17 20:56:47 +0800

    [Modification] - Modified hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function Hero() {
   return (
@@ -29,9 +30,13 @@ function Hero() {
         ></Image>
         <div class="backgrounds z-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex opacity-65 items-center justify-center"></div>
       </div>{" "}
-      <div className="w-[11rem] py-2 absolute flex items-center text-lg justify-center bottom-[12rem] rounded-xl text-white bg-[#83828A]">
+      <Link
+        href="https://www.linkedin.com/company/unravyl"
+        target="_blank"
+        className="w-[11rem] py-2 absolute flex hover:scale-105 transition items-center text-lg justify-center bottom-[12rem] rounded-xl text-white bg-[#83828A]"
+      >
         Avail Services <i class="bx bx-right-arrow-alt text-white text-xl"></i>
-      </div>
+      </Link>
     </section>
   );
 }
