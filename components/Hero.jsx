7d0bf4ee92d08ae2ad043c4af40762b30133e62d
Yunjin <jed.donaire08@gmail.com>
2024-06-17 19:25:53 +0800

    [Modification] - added hero page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 function Hero() {
   return (
     <section className="w-full flex flex-col h-[50rem] items-center justify-start relative">
+      <h1 className="text-4xl font-black">UNRAVYL</h1>
       <div className="profileCircle rounded-full flex items-center justify-center">
         <Image
           alt="bgProfile"
@@ -13,7 +14,7 @@ function Hero() {
           className="size-[5rem]"
         ></Image>
       </div>
-      <div className=" flex  flex-col mt-[4rem] items-center justify-center text-[7rem] font-black">
+      <div className=" flex headers  flex-col mt-[4rem] items-center justify-center text-[7rem] font-black">
         <h1>FREELANCE</h1>
         <h1>WEB</h1>
         <h1>DEVELOPERS</h1>
@@ -28,7 +29,7 @@ function Hero() {
         ></Image>
         <div class="backgrounds z-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex opacity-65 items-center justify-center"></div>
       </div>{" "}
-      <div className="w-[11rem] py-3 absolute flex items-center text-xl justify-center bottom-0 rounded-xl text-white bg-[#83828A]">
+      <div className="w-[11rem] py-2 absolute flex items-center text-lg justify-center bottom-[12rem] rounded-xl text-white bg-[#83828A]">
         Avail Services <i class="bx bx-right-arrow-alt text-white text-xl"></i>
       </div>
     </section>
