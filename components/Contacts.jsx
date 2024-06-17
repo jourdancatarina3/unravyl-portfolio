ed4cebe9cad1d3ca63fa3062e207643b5378d40e
Yunjin <jed.donaire08@gmail.com>
2024-06-17 19:25:27 +0800

    [Modification] - added contactus

---
+++ components/Contacts.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { projects } from "../constants";
+import Image from "next/image";
+import Link from "next/link";
+
+function Contacts() {
+  return (
+    <section className="flex items-center relative justify-center flex-col mt-[5rem]">
+      <div class="backgrounds z-0 absolute top-[5rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex opacity-65 items-center justify-center"></div>
+      <h1 className="text-[5rem] font-black header mb-[5rem]">Contacts</h1>
+      <div class="containerGlow relative">
+        <i class="bx bx-network-chart text-[#7b2ff7] text-[20rem] absolute top-[-8rem] right-[-8rem] z-[40]"></i>
+        <div class="glow"></div>
+        <div class="inner-container">
+          <div className="flex items-center  gap-5 justify-start">
+            <div class="circle-container">
+              <Image
+                src="/logo.png"
+                alt="Logo"
+                height={2490}
+                width={2404}
+                class="logo size-[4rem]"
+              ></Image>
+            </div>
+            <div class="status-badge ">
+              <div class="dot"></div>
+              <div class="text">Available for work</div>
+            </div>
+          </div>
+          <div className="flex gap-4 items-center mt-7 justify-start flex-row mb-[2rem]">
+            <div className="flex pt-[1rem] items-start w-[55%] justify-center flex-col">
+              {" "}
+              <p className="text-white font-bold mb-4">
+                Drop us a line or say on social networks
+              </p>
+              <p className="text-justify text-gray-400">
+                Whether you're a technology corporate in need of support or an
+                early-stage company looking to level up your design and building
+                software application, we'd love to talk about your project
+              </p>
+            </div>
+            <div className="flex h-full mt-[-2rem] items-start justify-start gap-4 flex-col">
+              <div className="flex  items-center gap-3 justify-start">
+                <i class="bx bxl-github text-[2rem]"></i>
+                <i class="bx bxl-linkedin-square text-[2rem]"></i>
+              </div>
+              <a href="#" class="contactButton">
+                Let's create magic together
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="profileCircle mt-[3rem] rounded-full flex items-center justify-center">
+        <Image
+          alt="bgProfile"
+          src="/logo.png"
+          height={1240}
+          width={1240}
+          className="size-[5rem]"
+        ></Image>
+      </div>
+      <h1 className="text-gray-400 my-[2rem] text-center">©UNRAVYL 2024</h1>
+    </section>
+  );
+}
+
+export default Contacts;
