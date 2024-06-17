85afb0e7d59232643d3f7710b9078eb506500246
Yunjin <jed.donaire08@gmail.com>
2024-06-17 12:59:10 +0800

    [Build][Jed] - Added here section

---
+++ components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+
+function Hero() {
+  return (
+    <section className="w-full flex flex-col h-[50rem] items-center justify-start relative">
+      <div className="profileCircle rounded-full flex items-center justify-center">
+        <Image
+          alt="bgProfile"
+          src="/logo.png"
+          height={1240}
+          width={1240}
+          className="size-[5rem]"
+        ></Image>
+      </div>
+      <div className=" flex  flex-col mt-[4rem] items-center justify-center text-[7rem] font-black">
+        <h1>FREELANCE</h1>
+        <h1>WEB</h1>
+        <h1>DEVELOPERS</h1>
+      </div>
+      <div className="w-full absolute top-0 right-0">
+        <Image
+          src="/bg.png"
+          alt="bg"
+          height={21240}
+          width={21240}
+          className="w-full opacity-35 z-10"
+        ></Image>
+        <div class="backgrounds z-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex opacity-65 items-center justify-center"></div>
+      </div>{" "}
+      <div className="w-[11rem] py-3 absolute flex items-center text-xl justify-center bottom-0 rounded-xl text-white bg-[#83828A]">
+        Avail Services <i class="bx bx-right-arrow-alt text-white text-xl"></i>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
