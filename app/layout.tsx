fcacee90fc32b8dd489f8a1bafb6905d9b0192f3
Yunjin <jed.donaire08@gmail.com>
2024-06-17 12:58:02 +0800

    [Feat][Jed] - Added font

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
