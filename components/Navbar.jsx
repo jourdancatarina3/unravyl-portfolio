4ad175d66a9cb7b44335f54c70c15ee31a030f9c
Yunjin <jed.donaire08@gmail.com>
2024-06-17 20:56:59 +0800

    [Modification] - Modified navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,14 +2,19 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { navLinks } from "../constants";
+import Link from "next/link";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full text-[#616161] flex justify-between px-10 py-6 items-center navbar">
       <div className="flex  items-center gap-5 justify-center">
-        <i class="bx bxl-github text-[2rem]"></i>
-        <i class="bx bxl-linkedin-square text-[2rem]"></i>
+        <Link href="https://github.com/unravyl/" target="_blank">
+          <i class="bx bxl-github text-[2rem]"></i>
+        </Link>
+        <Link href="https://www.linkedin.com/company/unravyl/" target="_blank">
+          <i class="bx bxl-linkedin-square text-[2rem]"></i>
+        </Link>
       </div>
       <ul className="list-none sm:flex  hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
