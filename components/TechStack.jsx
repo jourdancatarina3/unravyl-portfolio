ba87b4420ee81ea846b443f142ae2af49785c561
Yunjin <jed.donaire08@gmail.com>
2024-06-24 23:07:03 +0800

    [Feat][Jed]- Revert hidden and make a bg color for it

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -49,14 +49,14 @@ function TechStack() {
               onMouseEnter={() => handleHover(index)}
               onMouseLeave={handleMouseLeave}
               height={110}
-              className=" object-cover w-[6rem] sm:w-[7rem] md:w-[9rem] lg:w-[10.5rem] xl:w-[11rem] mb-2 hover:scale-110 transition"
+              className=" object-cover z-[-10] w-[6rem] sm:w-[7rem] md:w-[9rem] lg:w-[10.5rem] xl:w-[11rem] mb-2 hover:scale-110 transition"
             />
             {projectHovered === index && (
-              <div className="hidden md:block absolute   -bottom-20 text-2xl duration-500 ">
-                <h1 className="text-lg sm:text-xl lg:text-2xl font-bold text-center">
+              <div className="block z-[100] sm:bg-transparent bg-[#6d28d9] px-1 py-1 rounded-md  -bottom-20 text-2xl duration-500 ">
+                <h1 className="text-sm sm:text-xl lg:text-2xl font-bold text-center">
                   {stack.name}
                 </h1>
-                <p className="text-lg text-center">{stack.use}</p>
+                <p className="text-sm text-center">{stack.use}</p>
               </div>
             )}
           </div>
